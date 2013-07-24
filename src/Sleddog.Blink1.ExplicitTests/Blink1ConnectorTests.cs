ceb0a9779ba1e2f28ba5305725273d6d5ea887fe
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-24 23:35:23 +0200

    Added explicit test for reading serial

--- src/Sleddog.Blink1.ExplicitTests/Blink1ConnectorTests.cs
+++ src/Sleddog.Blink1.ExplicitTests/Blink1ConnectorTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Xunit;
 
@@ -14,6 +16,24 @@ namespace Sleddog.Blink1.ExplicitTests
 			Assert.DoesNotThrow(() => sut.Identify(TimeSpan.FromSeconds(1)));
 
 			Thread.Sleep(TimeSpan.FromSeconds(1));
+		}
+	}
+
+	public class Blink1Tests
+	{
+		private const string LowestSerialNumber = "0x1A001000";
+		private const string HighestSerialNumber = "0x1A002FFF";
+
+		[RequireBlink1Hardware]
+		public void ReadSerialReadsValidSerialNumber()
+		{
+			var connector = new Blink1Connector();
+
+			var blink1 = connector.Scan().First();
+
+			var actual = blink1.ReadSerial();
+
+			Assert.InRange(actual, LowestSerialNumber, HighestSerialNumber);
 		}
 	}
 }
