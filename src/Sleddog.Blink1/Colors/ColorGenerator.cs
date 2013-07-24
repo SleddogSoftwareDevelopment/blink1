562d72be4bcb8e313c80327e17f35af004b0eddd
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-24 20:43:05 +0200

    Added colorgenerator

---
+++ src/Sleddog.Blink1/Colors/ColorGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Sleddog.Blink1.Colors
+{
+	public class ColorGenerator
+	{
+		private static readonly Random Random = new Random();
+
+		public List<Color> GenerateColors(int colorCount)
+		{
+			Debug.Listeners.Add(new DefaultTraceListener());
+
+			var colors = new List<Color>();
+
+			var cellRange = 1f/colorCount;
+			var cellOffset = Random.NextDouble()*cellRange;
+
+			for (var i = 0; i < colorCount; i++)
+			{
+				var newHue = cellRange*i + cellOffset;
+
+				if (newHue > 1)
+					newHue -= 1;
+
+				newHue *= 360;
+
+				var hue = Convert.ToUInt16(newHue);
+
+				var hslColor = new HSL(hue, 0.3f, 0.4f);
+
+				colors.Add(hslColor);
+			}
+
+			return colors;
+		}
+	}
+}
