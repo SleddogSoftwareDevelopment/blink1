9842741d0639164fbff9450d95ee61fec8c5c0f0
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-24 18:36:02 +0200

    Added HSL 2 RGB conversion

--- src/Sleddog.Blink1/Colors/HSL.cs
+++ src/Sleddog.Blink1/Colors/HSL.cs
@@ -6,18 +6,18 @@ namespace Sleddog.Blink1.Colors
 	internal class HSL
 	{
 		public ushort Hue { get; private set; }
-		public ushort Saturation { get; private set; }
-		public ushort Luminance { get; private set; }
+		public float Saturation { get; private set; }
+		public float Luminance { get; private set; }
 
-		public HSL(ushort hue, ushort saturation, ushort luminance)
+		public HSL(ushort hue, float saturation, float luminance)
 		{
 			if (hue > 360)
 				throw new ArgumentOutOfRangeException("hue");
 
-			if (saturation > 100)
+			if (saturation < 0 || saturation > 1)
 				throw new ArgumentOutOfRangeException("saturation");
 
-			if (luminance > 100)
+			if (luminance < 0 || luminance > 1)
 				throw new ArgumentOutOfRangeException("luminance");
 
 			Hue = hue;
@@ -27,19 +27,44 @@ namespace Sleddog.Blink1.Colors
 
 		public static implicit operator Color(HSL hsl)
 		{
-			if (hsl.Saturation == 0)
-			{
-				var colorValue = (float) hsl.Luminance/100*255;
+			var hue = hsl.Hue;
+			var saturation = hsl.Saturation;
+			var luminance = hsl.Luminance;
 
-				var intColorValue = Convert.ToInt32(colorValue);
+			if (luminance.Equals(0))
+				return Color.FromArgb(0, 0, 0);
 
-				return Color.FromArgb(intColorValue, intColorValue, intColorValue);
-			}
-			else
-			{
-			}
+			if (luminance.Equals(100))
+				return Color.FromArgb(255, 255, 255);
 
-			return Color.Red;
+			var chroma = (1 - Math.Abs(2*luminance - 1))*saturation;
+			var hSection = hue/60f;
+			var x = chroma*(1 - Math.Abs(hSection%2 - 1));
+
+			var rgbValues = Tuple.Create<float, float, float>(0, 0, 0);
+
+			if (hSection <= 0 && hSection < 1)
+				rgbValues = new Tuple<float, float, float>(chroma, x, 0);
+			else if (hSection <= 1 && hSection < 2)
+				rgbValues = new Tuple<float, float, float>(x, chroma, 0);
+			else if (hSection <= 2 && hSection < 3)
+				rgbValues = new Tuple<float, float, float>(0, chroma, x);
+			else if (hSection <= 3 && hSection < 4)
+				rgbValues = new Tuple<float, float, float>(0, x, chroma);
+			else if (hSection <= 4 && hSection < 5)
+				rgbValues = new Tuple<float, float, float>(x, 0, chroma);
+			else if (hSection <= 5 && hSection < 6)
+				rgbValues = new Tuple<float, float, float>(chroma, 0, x);
+
+			var m = luminance - 0.5f*chroma;
+
+			rgbValues = Tuple.Create(rgbValues.Item1 + m, rgbValues.Item2 + m, rgbValues.Item3 + m);
+
+			var r = Convert.ToInt32(rgbValues.Item1*255);
+			var g = Convert.ToInt32(rgbValues.Item2*255);
+			var b = Convert.ToInt32(rgbValues.Item3*255);
+
+			return Color.FromArgb(r, g, b);
 		}
 	}
 }
