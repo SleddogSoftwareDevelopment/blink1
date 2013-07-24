9ac6513c247bf613e741ab16d7b8096a4e5e3094
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-24 23:37:59 +0200

    Moved Blink1 explicit tests into it's own file

---
+++ src/Sleddog.Blink1.ExplicitTests/Blink1Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Sleddog.Blink1.ExplicitTests
+{
+	public class Blink1Tests
+	{
+		private const string LowestSerialNumber = "0x1A001000";
+		private const string HighestSerialNumber = "0x1A002FFF";
+
+		[RequireBlink1Hardware]
+		public void ReadSerialReadsValidSerialNumber()
+		{
+			var connector = new Blink1Connector();
+
+			var blink1 = connector.Scan().First();
+
+			var actual = blink1.ReadSerial();
+
+			Assert.InRange(actual, LowestSerialNumber, HighestSerialNumber);
+		}
+	}
+}
