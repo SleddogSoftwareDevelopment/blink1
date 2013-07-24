133dc04ee75f2f1d1fa5f3f81bf5ea05cafc32ac
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-25 00:50:49 +0200

    Changed RequireBlink1HardwareAttribute to depende directly on HidLibrary instead of using Blink1Connector

--- src/Sleddog.Blink1.ExplicitTests/RequireBlink1HardwareAttribute.cs
+++ src/Sleddog.Blink1.ExplicitTests/RequireBlink1HardwareAttribute.cs
@@ -1,12 +1,20 @@
 ﻿using System.Linq;
+using HidLibrary;
 
 namespace Sleddog.Blink1.ExplicitTests
 {
 	public sealed class RequireBlink1HardwareAttribute : ExplicitFactAttribute
 	{
+		private const int VendorId = 0x27B8;
+		private const int ProductId = 0x01ED;
+
 		public RequireBlink1HardwareAttribute()
 		{
-			if (!new Blink1Connector().Scan().Any())
+			var hidDevices = HidDevices.Enumerate(VendorId, ProductId);
+
+			var devices = hidDevices as HidDevice[] ?? hidDevices.ToArray();
+
+			if (!devices.Any())
 				Skip = "No Blink1 units connected";
 		}
 	}
