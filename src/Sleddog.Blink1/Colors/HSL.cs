f5d3313992c9fdd1bc2d0b83697217f864143541
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2013-07-24 19:31:56 +0200

    Fixed HSL 2 RGB conversion

--- src/Sleddog.Blink1/Colors/HSL.cs
+++ src/Sleddog.Blink1/Colors/HSL.cs
@@ -41,28 +41,28 @@ namespace Sleddog.Blink1.Colors
 			var hSection = hue/60f;
 			var x = chroma*(1 - Math.Abs(hSection%2 - 1));
 
-			var rgbValues = Tuple.Create<float, float, float>(0, 0, 0);
+			var rgbValues = Tuple.Create(0f, 0f, 0f);
 
-			if (hSection <= 0 && hSection < 1)
-				rgbValues = new Tuple<float, float, float>(chroma, x, 0);
-			else if (hSection <= 1 && hSection < 2)
-				rgbValues = new Tuple<float, float, float>(x, chroma, 0);
-			else if (hSection <= 2 && hSection < 3)
-				rgbValues = new Tuple<float, float, float>(0, chroma, x);
-			else if (hSection <= 3 && hSection < 4)
-				rgbValues = new Tuple<float, float, float>(0, x, chroma);
-			else if (hSection <= 4 && hSection < 5)
-				rgbValues = new Tuple<float, float, float>(x, 0, chroma);
-			else if (hSection <= 5 && hSection < 6)
-				rgbValues = new Tuple<float, float, float>(chroma, 0, x);
+			if (hSection >= 0 && hSection < 1)
+				rgbValues = Tuple.Create(chroma, x, 0f);
+			else if (hSection >= 1 && hSection < 2)
+				rgbValues = Tuple.Create(x, chroma, 0f);
+			else if (hSection >= 2 && hSection < 3)
+				rgbValues = Tuple.Create(0f, chroma, x);
+			else if (hSection >= 3 && hSection < 4)
+				rgbValues = Tuple.Create(0f, x, chroma);
+			else if (hSection >= 4 && hSection < 5)
+				rgbValues = Tuple.Create(x, 0f, chroma);
+			else if (hSection >= 5 && hSection < 6)
+				rgbValues = Tuple.Create(chroma, 0f, x);
 
 			var m = luminance - 0.5f*chroma;
 
-			rgbValues = Tuple.Create(rgbValues.Item1 + m, rgbValues.Item2 + m, rgbValues.Item3 + m);
+			var modRGBValues = Tuple.Create(rgbValues.Item1 + m, rgbValues.Item2 + m, rgbValues.Item3 + m);
 
-			var r = Convert.ToInt32(rgbValues.Item1*255);
-			var g = Convert.ToInt32(rgbValues.Item2*255);
-			var b = Convert.ToInt32(rgbValues.Item3*255);
+			var r = (int) Math.Floor(modRGBValues.Item1*255);
+			var g = (int) Math.Floor(modRGBValues.Item2*255);
+			var b = (int) Math.Floor(modRGBValues.Item3*255);
 
 			return Color.FromArgb(r, g, b);
 		}
