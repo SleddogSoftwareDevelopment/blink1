492ed81f13124d9303d155f2dc3118ce09004ae6
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-04-02 20:33:33 +0200

    Refactored start/stop preset playback for v1 and mk2

--- src/Sleddog.Blink1/Blink1Mk2.cs
+++ src/Sleddog.Blink1/Blink1Mk2.cs
@@ -3,69 +3,10 @@ using System.Drawing;
 
 namespace Sleddog.Blink1
 {
-    public class Blink1Mk2:IBlink1Mk2,IDisposable
+    public class Blink1Mk2 : Blink1, IBlink1Mk2, IDisposable
     {
-        public Version Version { get; private set; }
-        public string SerialNumber { get; private set; }
-
-        public bool Blink(Color color, TimeSpan interval, ushort times)
+        internal Blink1Mk2(Blink1CommandBus commandBus) : base(commandBus)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool SetColor(Color color)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool FadeToColor(Color color, TimeSpan fadeDuration)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ShowColor(Color color, TimeSpan visibleTime)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool SavePreset(Blink1Preset preset, ushort position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Blink1Preset ReadPreset(ushort position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool PlaybackPresets(ushort startPosition)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool PausePresets()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool EnableInactivityMode(TimeSpan waitDuration)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool DisableInactivityMode()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void TurnOff()
-        {
-            throw new NotImplementedException();
-        }
-
-        void IBlink1.Dispose()
-        {
-            throw new NotImplementedException();
         }
 
         public bool SetColor(Color color, LEDPosition ledPosition)
@@ -79,11 +20,6 @@ namespace Sleddog.Blink1
         }
 
         public PlaybackStatus ReadPlaybackStatus()
-        {
-            throw new NotImplementedException();
-        }
-
-        void IDisposable.Dispose()
         {
             throw new NotImplementedException();
         }
--- src/Sleddog.Blink1/Commands/PlayPresetCommand.cs
+++ src/Sleddog.Blink1/Commands/PlayPresetCommand.cs
@@ -6,10 +6,19 @@ namespace Sleddog.Blink1.Commands
     internal class PlayPresetCommand : IBlink1Command
     {
         private readonly byte startPosition;
+	    private readonly byte endPosition;
+	    private readonly byte count;
 
-        public PlayPresetCommand(ushort startPosition)
+	    public PlayPresetCommand(ushort startPosition) : this(startPosition, 0, 0)
+	    {
+
+	    }
+
+	    public PlayPresetCommand(ushort startPosition, ushort endPosition, ushort count)
         {
             this.startPosition = Convert.ToByte(startPosition);
+	        this.endPosition = Convert.ToByte(endPosition);
+	        this.count = Convert.ToByte(count);
         }
 
         public byte[] ToHidCommand()
@@ -20,6 +29,10 @@ namespace Sleddog.Blink1.Commands
                 (byte) Blink1Commands.PresetControl,
                 Convert.ToByte(true),
                 startPosition
+                       endPosition,
+                       count,
+                       Convert.ToByte(0),
+                       Convert.ToByte(0)
             };
         }
     }
