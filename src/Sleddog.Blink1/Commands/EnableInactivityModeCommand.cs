69893753d9c35863772556260d71419beb64335f
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-04-30 09:12:54 +0200

    Added mk2 features to ServerMode/Inactivity mode

--- src/Sleddog.Blink1/Commands/EnableInactivityModeCommand.cs
+++ src/Sleddog.Blink1/Commands/EnableInactivityModeCommand.cs
@@ -3,25 +3,40 @@ using Sleddog.Blink1.Interfaces;
 
 namespace Sleddog.Blink1.Commands
 {
-	internal class EnableInactivityModeCommand : IBlink1Command
-	{
-		private readonly Blink1Duration waitDuration;
+    internal class EnableInactivityModeCommand : IBlink1Command
+    {
+        private readonly Blink1Duration waitDuration;
+        private readonly bool maintainState;
+        private readonly ushort startPosition;
+        private readonly ushort endPosition;
 
-		public EnableInactivityModeCommand(Blink1Duration waitDuration)
-		{
-			this.waitDuration = waitDuration;
-		}
+        public EnableInactivityModeCommand(Blink1Duration waitDuration)
+        {
+            this.waitDuration = waitDuration;
+        }
 
-		public byte[] ToHidCommand()
-		{
-			return new[]
-			       {
-				       Convert.ToByte(1),
-				       (byte) Blink1Commands.InactivityMode,
-				       Convert.ToByte(true),
-				       waitDuration.High,
-				       waitDuration.Low
-			       };
-		}
-	}
+        public EnableInactivityModeCommand(Blink1Duration waitDuration, bool maintainState, ushort startPosition,
+            ushort endPosition)
+        {
+            this.waitDuration = waitDuration;
+            this.maintainState = maintainState;
+            this.startPosition = startPosition;
+            this.endPosition = endPosition;
+        }
+
+        public byte[] ToHidCommand()
+        {
+            return new[]
+            {
+                Convert.ToByte(1),
+                (byte) Blink1Commands.InactivityMode,
+                Convert.ToByte(true),
+                waitDuration.High,
+                waitDuration.Low,
+                Convert.ToByte(maintainState),
+                Convert.ToByte(startPosition),
+                Convert.ToByte(endPosition)
+            };
+        }
+    }
 }
