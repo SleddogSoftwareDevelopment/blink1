eab5723967d2751f62a44e708f9a35f67db54023
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-04-30 08:10:02 +0200

    Added LEDPosition option for FadeToColor

--- src/Sleddog.Blink1/Commands/FadeToColorCommand.cs
+++ src/Sleddog.Blink1/Commands/FadeToColorCommand.cs
@@ -4,29 +4,36 @@ using Sleddog.Blink1.Interfaces;
 
 namespace Sleddog.Blink1.Commands
 {
-	internal class FadeToColorCommand : IBlink1Command
-	{
-		private readonly Color color;
-		private readonly Blink1Duration duration;
+    internal class FadeToColorCommand : IBlink1Command
+    {
+        private readonly Color color;
+        private readonly Blink1Duration duration;
+        private readonly LEDPosition ledPosition;
 
-		public FadeToColorCommand(Color color, Blink1Duration duration)
-		{
-			this.color = color;
-			this.duration = duration;
-		}
+        public FadeToColorCommand(Color color, Blink1Duration duration) : this(color, duration, LEDPosition.Both)
+        {
+        }
 
-		public byte[] ToHidCommand()
-		{
-			return new[]
-			       {
-				       Convert.ToByte(1),
-				       (byte) Blink1Commands.FadeToColor,
-				       color.R,
-				       color.G,
-				       color.B,
-				       duration.High,
-				       duration.Low
-			       };
-		}
-	}
+        public FadeToColorCommand(Color color, Blink1Duration duration, LEDPosition ledPosition)
+        {
+            this.color = color;
+            this.duration = duration;
+            this.ledPosition = ledPosition;
+        }
+
+        public byte[] ToHidCommand()
+        {
+            return new[]
+            {
+                Convert.ToByte(1),
+                (byte) Blink1Commands.FadeToColor,
+                color.R,
+                color.G,
+                color.B,
+                duration.High,
+                duration.Low,
+                Convert.ToByte(ledPosition)
+            };
+        }
+    }
 }
