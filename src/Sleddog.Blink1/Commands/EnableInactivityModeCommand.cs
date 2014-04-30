0519f3f85037e601705783ca9f1db58ed650d5b0
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-04-30 09:12:54 +0200

    Added mk2 features to ServerMode/Inactivity mode

--- src/Sleddog.Blink1/Commands/EnableInactivityModeCommand.cs
+++ src/Sleddog.Blink1/Commands/EnableInactivityModeCommand.cs
@@ -6,10 +6,22 @@ namespace Sleddog.Blink1.Commands
     internal class EnableInactivityModeCommand : IBlink1Command
     {
         private readonly Blink1Duration waitDuration;
+        private readonly bool maintainState;
+        private readonly ushort startPosition;
+        private readonly ushort endPosition;
 
         public EnableInactivityModeCommand(Blink1Duration waitDuration)
         {
             this.waitDuration = waitDuration;
+        }
+
+        public EnableInactivityModeCommand(Blink1Duration waitDuration, bool maintainState, ushort startPosition,
+            ushort endPosition)
+        {
+            this.waitDuration = waitDuration;
+            this.maintainState = maintainState;
+            this.startPosition = startPosition;
+            this.endPosition = endPosition;
         }
 
         public byte[] ToHidCommand()
@@ -20,7 +32,10 @@ namespace Sleddog.Blink1.Commands
                 (byte) Blink1Commands.InactivityMode,
                 Convert.ToByte(true),
                 waitDuration.High,
-                waitDuration.Low
+                waitDuration.Low,
+                Convert.ToByte(maintainState),
+                Convert.ToByte(startPosition),
+                Convert.ToByte(endPosition)
             };
         }
     }
