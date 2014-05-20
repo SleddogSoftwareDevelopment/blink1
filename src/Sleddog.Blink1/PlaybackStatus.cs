961a7a77a089f3419fed6e3f4fafb209dc408680
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-05-20 18:41:46 +0200

    Added ReadPlaybackState command to mk2

--- src/Sleddog.Blink1/PlaybackStatus.cs
+++ src/Sleddog.Blink1/PlaybackStatus.cs
@@ -2,10 +2,19 @@
 {
     public class PlaybackStatus
     {
-        public bool IsPlaying { get; set; }
-        public ushort StartPosition { get; set; }
-        public ushort EndPosition { get; set; }
-        public ushort Count { get; set; }
-        public ushort Position { get; set; }
+        public bool IsPlaying { get; private set; }
+        public ushort StartPosition { get; private set; }
+        public ushort EndPosition { get; private set; }
+        public ushort Count { get; private set; }
+        public ushort Position { get; private set; }
+
+        internal PlaybackStatus(bool isPlaying, ushort startPosition, ushort endPosition, ushort count, ushort position)
+        {
+            IsPlaying = isPlaying;
+            StartPosition = startPosition;
+            EndPosition = endPosition;
+            Count = count;
+            Position = position;
+        }
     }
 }
