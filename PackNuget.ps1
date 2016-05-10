0eaa2f0a10903201d023c8bf76f2381df234375d
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2016-05-10 21:49:58 +0200

    Updated script to run more smoothly

--- PackNuget.ps1
+++ PackNuget.ps1
@@ -1,5 +1,7 @@
 Remove-Item -Force -ErrorAction SilentlyContinue .\Build\*.nupkg
 
-.\Nuget.exe pack -OutputDirectory .\Build .\src\Sleddog.Blink1\Sleddog.Blink1.csproj
+New-Item -ItemType Directory Build
+
+.\Nuget.exe pack -OutputDirectory .\Build .\src\Sleddog.Blink1\Sleddog.Blink1.csproj -Prop Configuration=Release
 
 #.\src\.nuget\Nuget.exe push .\Build\*.nupkg
