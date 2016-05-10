537821734e70631ad857978737e351cb8919b361
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2016-05-10 20:11:23 +0200

    Modifications to nuget packer

--- PackNuget.ps1
+++ PackNuget.ps1
@@ -1,5 +1,5 @@
-rm -Force .\Build\*.nupkg
+Remove-Item -Force -ErrorAction SilentlyContinue .\Build\*.nupkg
 
-.\src\.nuget\Nuget.exe pack -OutputDirectory .\Build .\src\Sleddog.Blink1\Sleddog.Blink1.csproj
+.\Nuget.exe pack -OutputDirectory .\Build .\src\Sleddog.Blink1\Sleddog.Blink1.csproj
 
-.\src\.nuget\Nuget.exe push .\Build\*.nupkg
+#.\src\.nuget\Nuget.exe push .\Build\*.nupkg
