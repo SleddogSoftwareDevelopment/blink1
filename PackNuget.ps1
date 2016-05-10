27fd505da65544f1cda4751ab9ee51e49afd4fa6
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2016-05-10 22:01:55 +0200

    Finalized nuget script

--- PackNuget.ps1
+++ PackNuget.ps1
@@ -1,7 +1,7 @@
-Remove-Item -Force -ErrorAction SilentlyContinue .\Build\*.nupkg
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue .\Build
 
 New-Item -ItemType Directory Build
 
 .\Nuget.exe pack -OutputDirectory .\Build .\src\Sleddog.Blink1\Sleddog.Blink1.csproj -Prop Configuration=Release
 
-#.\src\.nuget\Nuget.exe push .\Build\*.nupkg
+.\Nuget.exe push -Source https://www.nuget.org/api/v2/package .\Build\*.nupkg
