fcbcf931a6d67edf61327611da163d320438f376
Stefan Daugaard Poulsen <sdp@ipaper.io>
2019-12-22 12:40:27 +0100

    Added reference for mk3 commands

--- src/Sleddog.Blink1/Internal/Blink1Commands.cs
+++ src/Sleddog.Blink1/Internal/Blink1Commands.cs
@@ -1,16 +1,19 @@
 namespace Sleddog.Blink1.Internal
 {
-    internal enum Blink1Commands : byte
-    {
-        Test = (byte) '!',
-        GetVersion = (byte) 'v',
-        SetColor = (byte) 'n',
-        FadeToColor = (byte) 'c',
-        InactivityMode = (byte) 'D',
-        PresetControl = (byte) 'p',
-        ReadPreset = (byte) 'R',
-        SavePreset = (byte) 'P',
-        SavePresetMk2 = (byte) 'W',
-        ReadPlaybackState = (byte) 'S',
-    }
+	/*
+   * mk3 commands: https://github.com/todbot/blink1mk3/blob/master/firmware/firmware-v30x/main.c#L968
+   */
+	internal enum Blink1Commands : byte
+	{
+		Test = (byte) '!',
+		GetVersion = (byte) 'v',
+		SetColor = (byte) 'n',
+		FadeToColor = (byte) 'c',
+		InactivityMode = (byte) 'D',
+		PresetControl = (byte) 'p',
+		ReadPreset = (byte) 'R',
+		SavePreset = (byte) 'P',
+		SavePresetMk2 = (byte) 'W',
+		ReadPlaybackState = (byte) 'S'
+	}
 }
